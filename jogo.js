f02d8f4690e9feb1721fddf8d976c9f40e3772f1
GabrielHenriqueRS <38798917+GabrielHenriqueRS@users.noreply.github.com>
2018-04-28 15:31:51 -0300

    Identação 2

--- jogo.js
+++ jogo.js
@@ -32,7 +32,7 @@ somFundo = new Audio('Sons/fundo.mp3');
 somFundo.play();
 somFundo.loop = true;
 
-//Variáveis do fundo
+//Variáveis da tela de fundo
 var fundo = new Image();
 fundo.src = 'telafundo.png';
 
@@ -66,19 +66,20 @@ function Desenhar(){
 
     anterior = agora;
 
-    if(gameOver==true){
+    if(gameOver || win ==true){
         cancelAnimationFrame(Desenhar);
     } else {
         requestAnimationFrame(Desenhar);
     }
 
-    if(win==true){
+    /*if(win==true){
         cancelAnimationFrame(Desenhar);
     } else {
         requestAnimationFrame(Desenhar);
-    }	
+    }*/	
 }
 
+//Placar de pontos
 function desenhaPontos(){
     context.fillStyle = "White";
     context.font = "16pt Monospace";
@@ -86,6 +87,7 @@ function desenhaPontos(){
     context.fillText(pontos, 130, 20);
 }
 
+//Placar de pontos
 function desenhaVidas(){
     context.fillStyle = "Red";
     context.font = "16pt Monospace";
@@ -93,51 +95,66 @@ function desenhaVidas(){
     context.fillText(vidas, 80, 50);
 }
 
+//Fim com derrota
 function GameOver(){
     if(gameOver==true){
         var fundo = new Image();
         fundo.src = 'Telas/derrota.jpg';
+
         fundo.onload = function(){
             context.drawImage(fundo, 0, 0, 1280, 900);
         }
+
         somFundo.pause();
         somFundo.currentTime = 0;
+
         somFim = new Audio('Sons/musicaD.mp3');
         somFim.play();
+
         explodirInimigo = false;
         desenhaVidas() = false;
         desenhaPontos() = false;
     }
 }
 
+//Fim com vitória
 function Win(){
     if(win==true){
         var fundo = new Image();
         fundo.src = 'Telas/vitoria.jpg';
+
         fundo.onload = function(){
             context.drawImage(fundo, 0, 0, 1280, 900);
         }
+
         somFundo.pause();
         somFundo.currentTime = 0;
+
         somFim = new Audio('Sons/musicaV.mp3');
         somFim.play();
+
         explodirInimigo = false;
         desenhaVidas() = false;
         desenhaPontos() = false;
+
     }
 }
 
+//Função da tela de fundo
 function Fundo(){
     context.drawImage(fundo,0,0,1002,702);				
     console.log( 'Fundo()' );
 }
 
+//Função da nave
 function Persona(){
     if (naveExibir == true) {
         var persona = new Image();
         persona.src = 'Nave3.png';
+
         altura = 58;
         largura = 186;
+
         eex = 1 * coluna;//58
         eey = 1 * linha;//122
 
@@ -151,6 +168,7 @@ function Persona(){
             largura, altura, xP, yP,
             largura * 1, altura * 1
         );
+
         /*if(quadro>9){
             if(frente==true)
                 coluna++;
@@ -180,36 +198,47 @@ function Persona(){
     }
 }
 
+//Função da nave inimiga
 function Inimigo(){
     var inimigo = new Image();
     if(tipoI==0)
         inimigo.src = 'naveInimiga.png';
+
     else if(tipoI==1)
         inimigo.src = 'naveInimiga.png';
+
     else if(tipoI==2)
         inimigo.src = 'naveInimiga.png';
+
     else if(tipoI==3)
         inimigo.src = 'naveInimiga.png';
+
     else if(tipoI==4)
         inimigo.src = 'naveInimiga.png';			
     
     inimigo.onload = function(){
         context.drawImage(inimigo,xI,yI,160,160);
     }
+
     xI-=velocidade;
     velocidade -= 0.000005;
+
     if(xI>1004){
         xI=1002;
         yI=Math.random()*500;
         tipoI = Math.round(Math.random()*4);
     }
+
     if(xI>1002 && xI < 0){
         if(yI > yP-93 && yI < yP+93){
             explodirInimigo = true;
             naveExibir = false;
+
             xBoom = 0;
             yBoom = yP+200;
+
             vidas--;
+
             if(vidas<1){
                 gameOver = true;
             }
@@ -217,14 +246,18 @@ function Inimigo(){
     }
 }
 
+//Função da colisão e animação
 function Explodir(vai_explodir){
     if(vai_explodir==true){
         var explode = new Image();
         explode.src = 'explosao.png';
+
         eex = 200 * coluna;
         eey = 200 * linha;
+
         altura = 200;
         largura = 200;
+
         explode.onload = function(){
             context.drawImage(
                 explode, eex, eey,
@@ -232,15 +265,19 @@ function Explodir(vai_explodir){
                 largura * 0.5, altura * 0.5
             );
         }
+
         quadro++;
+
         if(quadro > 6){
             coluna++;
             quadro = 0;
         }
+
         if(coluna ==4){
             linha++
             coluna = 0;
         }
+
         if(linha == 1 && coluna == 2){
             coluna = 0;
             linha = 0;
@@ -249,28 +286,37 @@ function Explodir(vai_explodir){
     }
 }
 
+//Função do tiro
 function Tiro(){
     this.xT = xP+186;
     this.yT = yP;
+
     this.mover = function(){
         context.fillStyle = '#e50000';
         context.fillRect(this.xT, this.yT, 20, 6);
         this.xT+=3*decorrido/10;
+
         if(this.xT<3){
             this.xT = 700;
             balas.splice(0,1);
         }
+
         if(this.yT>yI-93 && this.yT<yI+93){
             if(this.xT>xI && this.xT<xI+93){
                 explodirInimigo = true;
+
                 xBoom = xI;
                 yBoom = yI;
+
                 this.yT=700;
                 balas.splice(0,1);
+
                 xI=1002;
                 yI=Math.random()*500;
                 tipoI = Math.round(Math.random()*4);
+
                 pontos++;
+
                 if(pontos>=10){
                     win = true;
                 }					
@@ -279,73 +325,83 @@ function Tiro(){
     }
 }
 
+//Eventos do teclado quando não clicado
 document.addEventListener('keydown',function(evento){
 
-//seta Rigth
-if (evento.keyCode == 39)
-    if(xP<650){
-        xP+=8;
-        moverX=true;
-        frente=true;
+    //seta Right
+    if (evento.keyCode == 39){
+        if(xP<650){
+            xP+=8;
+            moverX=true;
+            frente=true;
+        }
     }
 
-//seta Left
-if (evento.keyCode == 37)
-    if(xP>0){
-        xP-=8;
-        moverX=true;
-        frente=false;
+    //seta Left
+    if (evento.keyCode == 37){
+        if(xP>0){
+            xP-=8;
+            moverX=true;
+            frente=false;
+        }
     }
 
-//seta Down
-if (evento.keyCode == 40)
-    if(yP<610){
-        yP+=8;
-        moverY=true;
-        cima=true;
+    //seta Down
+    if (evento.keyCode == 40){
+        if(yP<610){
+            yP+=8;
+            moverY=true;
+            cima=true;
+        }
     }
-    
-//seta Up
-if (evento.keyCode == 38)
-    if(yP>0){
-        yP-=8;
-        moverY=true;
-        cima=false;
+
+    //seta Up
+    if (evento.keyCode == 38){
+        if(yP>0){
+            yP-=8;
+            moverY=true;
+            cima=false;
+        }
     }
-//barra 
-if(evento.keyCode == 32){
-    balas[balas.length] = new Tiro();
-    console.log(somTiro.currentTime);
-    somTiro.play();
-}
+
+    //Barra de espaço
+    if(evento.keyCode == 32){
+        balas[balas.length] = new Tiro();
+        console.log(somTiro.currentTime);
+        somTiro.play();
+    }
 });
 
+//Eventos do teclado quando clicado
 document.addEventListener('keyup',function(evento){
-//seta Rigth
-if (evento.keyCode == 39){
+
+    //seta Right
+    if (evento.keyCode == 39){
         moverX=false;
         coluna=0;
         linha=1;
-}
-//seta Left
-if (evento.keyCode == 37){
+    }
+
+    //seta Left
+    if (evento.keyCode == 37){
         moverX=false;
         frente=false;
         coluna=0;
         linha=1;
-}
-//seta Up
-if (evento.keyCode == 40){
+    }
+
+    //seta Up
+    if (evento.keyCode == 40){
         moverY=false;
         coluna=0;
         linha=1;
     }
-//seta Down
-if (evento.keyCode == 38){
+
+    //seta Down
+    if (evento.keyCode == 38){
         moverY=false;
         cima=false;
         coluna=0;
-        linha=1;
-        
-}
+        linha=1;     
+    }
 });		
